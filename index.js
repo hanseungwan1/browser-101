16abd518420fb909233cee67adb3ad9f900f9740
hanseungwan1 <seungwanhan542@gmail.com>
2023-07-25 00:53:11 +0900

    feat: window 사이즈 출력 과제 완료

---
+++ index.js
@@ -0,0 +1,16 @@
+function getWindowSize() {
+  screen.innerHTML = `${window.screen.width}, ${window.screen.height}`;
+  outer.innerHTML = `${window.outerWidth}, ${window.outerHeight}`;
+  inner.innerHTML = `${window.innerWidth}, ${window.innerHeight}`;
+  client_width.innerHTML = `${document.documentElement.clientWidth}, ${document.documentElement.clientHeight}`;
+}
+
+const screen = document.querySelector(".screen");
+const outer = document.querySelector(".outer");
+const inner = document.querySelector(".inner");
+const client_width = document.querySelector(".client_width");
+getWindowSize();
+
+window.addEventListener("resize", function () {
+  getWindowSize();
+});
